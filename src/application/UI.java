9bd190947f7bbd64ed14fb40e5242aa3a8f7b95a
joaomcsferreira <marcosc024@gmail.com>
2023-06-22 15:27:35 -0300

    update: handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -39,6 +39,10 @@ public class UI {
         }
     }
 
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
